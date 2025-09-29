84c469088dbd3206e69b04a55f497880d8ca9def
TriDaK <trinekremmling@gmail.com>
2025-09-29 21:20:06 +0200

    expanded on hairdresser model

--- BackendAPI/BackendAPI/Models/Hairdresser.cs
+++ BackendAPI/BackendAPI/Models/Hairdresser.cs
@@ -3,5 +3,9 @@
     public class Hairdresser
     {
         public int ID { get; set; }
+        public string SalonName { get; set; }
+        public string Website { get; set; }  
+        public double Lat { get; set; }
+        public double Lng { get; set; }
     }
 }
