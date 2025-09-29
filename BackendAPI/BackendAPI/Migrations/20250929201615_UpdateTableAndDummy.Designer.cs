f1fb509384efdb8bfed2b886ad9845a55a5cb316
TriDaK <trinekremmling@gmail.com>
2025-09-29 22:22:28 +0200

    Changed dummy data in MaMADbContext, migrated to DB

--- BackendAPI/BackendAPI/Migrations/20250929201615_UpdateTableAndDummy.Designer.cs
+++ BackendAPI/BackendAPI/Migrations/20250929201615_UpdateTableAndDummy.Designer.cs
@@ -2,6 +2,7 @@
 using BackendAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BackendAPI.Migrations
 {
     [DbContext(typeof(MaMaDbContext))]
-    partial class MaMaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250929201615_UpdateTableAndDummy")]
+    partial class UpdateTableAndDummy
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BackendAPI/BackendAPI/Migrations/20250929201615_UpdateTableAndDummy.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BackendAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTableAndDummy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Lat",
+                table: "Hairdressers",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Lng",
+                table: "Hairdressers",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SalonName",
+                table: "Hairdressers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Website",
+                table: "Hairdressers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Hairdressers",
+                keyColumn: "ID",
+                keyValue: 1,
+                columns: new[] { "Lat", "Lng", "SalonName", "Website" },
+                values: new object[] { 55.710031999999998, 9.5347290000000005, "Hairværk", "https://hairvaerkvejle.dk/" });
+
+            migrationBuilder.UpdateData(
+                table: "Hairdressers",
+                keyColumn: "ID",
+                keyValue: 2,
+                columns: new[] { "Lat", "Lng", "SalonName", "Website" },
+                values: new object[] { 55.612772999999997, 9.5668050000000004, "Salon No. 24", "https://salonno24.dk/" });
+
+            migrationBuilder.InsertData(
+                table: "Hairdressers",
+                columns: new[] { "ID", "Lat", "Lng", "SalonName", "Website" },
+                values: new object[,]
+                {
+                    { 3, 55.711891000000001, 9.5365300000000008, "Frisør PARK Styling Vejle", "https://parkstyling.dk/pages/salon/vejle" },
+                    { 4, 55.710152000000001, 9.5352700000000006, "City Frisørerne", "https://parkstyling.dk/pages/salon/vejle-city" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hairdressers",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Hairdressers",
+                keyColumn: "ID",
+                keyValue: 4);
+
+            migrationBuilder.DropColumn(
+                name: "Lat",
+                table: "Hairdressers");
+
+            migrationBuilder.DropColumn(
+                name: "Lng",
+                table: "Hairdressers");
+
+            migrationBuilder.DropColumn(
+                name: "SalonName",
+                table: "Hairdressers");
+
+            migrationBuilder.DropColumn(
+                name: "Website",
+                table: "Hairdressers");
+        }
+    }
+}
