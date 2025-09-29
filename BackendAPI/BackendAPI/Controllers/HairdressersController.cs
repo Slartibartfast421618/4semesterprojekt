5b2a57a1fa6be86cde03859867bad802f06e7dbd
TriDaK <trinekremmling@gmail.com>
2025-09-29 21:06:41 +0200

    Basic get all hairdressers functioning. Testet on postman

---
+++ BackendAPI/BackendAPI/Controllers/HairdressersController.cs
@@ -0,0 +1,28 @@
+﻿using BackendAPI.Data;
+using BackendAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HairdressersController : ControllerBase
+    {
+        private readonly MaMaDbContext _context;
+
+        public HairdressersController(MaMaDbContext context)
+        {
+            _context = context;
+        }
+
+        // Get All Hairdressers in the DB
+        [HttpGet]
+        public async Task <ActionResult<IEnumerable<Hairdresser>>> GetAllHairdressers()
+        {
+            var hairdressers = new List<Hairdresser>();
+            hairdressers = await _context.Hairdressers.ToListAsync();
+            return Ok(hairdressers);
+        }
+    }
+}
