bedb213b8a15bc1003639b7934b8d4c2026cc1fa
TriDaK <trinekremmling@gmail.com>
2025-11-03 15:39:02 +0100

    Working convertion of rawdata to data the API can handle

--- CsvToDb/CsvToDb/CsvToDb.py
+++ CsvToDb/CsvToDb/CsvToDb.py
@@ -2,14 +2,34 @@ import pandas as pd
 
 # read csv fil - give directions to fil location
     # MUST BE SET FOR YOU PERSONALY
-df = pd.read_csv('C:\\Users\\trine\\source\\repos\\4semesterprojekt\\CsvToDb\\HairdresserCsv\\5FrisørMedHjemmesider.csv')
+csvFile = 'C:\\Users\\trine\\source\\repos\\4semesterprojekt\\CsvToDb\\HairdresserCsv\\5FrisørMedHjemmesider.csv'
 
-# Sort wanted columns
+# wanted columns from csvFile
 NeedColumns = ['Frisør' , 'Adresse' , 'Adresse 2' , 'Hjemmeside']
-df = df[NeedColumns]
+
+# data frame , df
+df = pd.read_csv(csvFile , delimiter=';', usecols=NeedColumns)
+
+# delete all datapoints with no webpage/hjemmeside
+df = df[df['Hjemmeside'].notnull() & (df['Hjemmeside'] != '')]
 
 # combine address sting 
+df['fullAddress'] = df['Adresse'] + ', ' + df['Adresse 2']
 
+# Build DTO to API call from rows in df
+def build_dto(row):
+    return {
+        "salonName": row['Frisør'],
+        "website": row['Hjemmeside'],
+        "address": row['fullAddress']
+        }
+
+dtoList = [build_dto(row) for _, row in df.iterrows()]
+
+# test to view the transformed data format
+for dto in dtoList: print (dto)
+
+# POST to API
 
 
 
