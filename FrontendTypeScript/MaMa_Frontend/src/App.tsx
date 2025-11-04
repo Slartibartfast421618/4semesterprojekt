82ebaeb47a627c8fea11918f9c84bfade31ec858
MHLa <mhla63314@edu.ucl.dk>
2025-11-04 11:50:16 +0100

    Navbar and Card extraction

--- FrontendTypeScript/MaMa_Frontend/src/App.tsx
+++ FrontendTypeScript/MaMa_Frontend/src/App.tsx
@@ -1,102 +1,42 @@
-import mamaLogo from './assets/manemanager_nbg_close.png'
-import fakeMap from './assets/fake_maps.png'
-import './App.css'
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Card from "./components/Card";
+import fakeMap from './assets/fake_maps.png';
+import './App.css';
 
 function App() {
 
-  return (
-      <>
-          <div className="navbar">
-                <a href="http://localhost:7000" target="_blank">
-                    <img src={mamaLogo} className="logo" alt="Mane Manager logo" />
-                </a>
-                <div className="stack textbox">
-                    <span className="name">Mane Manager</span>
-                    <span className="slogan">Mane management made mild</span>
-                </div>
-              <div className="spacer" />
-              <div className="stack">
-                  <span className="searchterm">Place</span>
-                  <input className="searchinput" />
-              </div>
-              <div className="stack">
-                  <span className="searchterm">TimeDate start</span>
-                  <input className="searchinput" />
-              </div>
-              <div className="stack">
-                  <span className="searchterm">TimeDate end</span>
-                  <input className="searchinput" />
-              </div>
-                <div className="spacer" />
-          </div>
-          <div className="split-container">
-              <div className="split left">
-                <p>Navn - Distance - Ledige tider - Priser fra</p>
+    const [place, setPlace] = useState("");
+    const [start, setStart] = useState("");
+    const [end, setEnd] = useState("");
 
-                  <div className="card">
-                      <div className="segment_info">
-                          <p>
-                              Kalle's Korte Klip
-                          </p>
-                      </div>
-                      <div className="segment_info">
-                          <p>
-                              3km
-                          </p>
-                      </div>
-                      <div className="segment_info">
-                          <p>
-                            3
-                          </p>
-                          <p>
-                              ledige tider
-                          </p>
-                      </div>
-                      <div className="splitter" />
-                      <div className="segment_price">
-                          <p>
-                              fra
-                          </p>
-                          <p className="price">
-                              99 kr
-                          </p>
-                          <button>
-                              Se mere
-                          </button>
-                      </div>
-                  </div>
-                  <div className="card">
-                      <div className="segment_info">
-                          <p>
-                              KlipKlapperne
-                          </p>
-                      </div>
-                      <div className="segment_info">
-                          <p>
-                              5km
-                          </p>
-                      </div>
-                      <div className="segment_info">
-                          <p>
-                            12
-                          </p>
-                          <p>
-                              ledige tider
-                          </p>
-                      </div>
-                      <div className="splitter" />
-                      <div className="segment_price">
-                          <p>
-                              fra
-                          </p>
-                          <p className="price">
-                              1324 kr
-                          </p>
-                          <button>
-                              Se mere
-                          </button>
-                      </div>
-                  </div>
+    return (
+        <>
+            <Navbar
+                place={place}
+                start={start}
+                end={end}
+                onPlaceChange={setPlace}
+                onStartChange={setStart}
+                onEndChange={setEnd}
+            />
+            <div className="split-container">
+                <div className="split left">
+                  <p>Navn - Distance - Ledige tider - Priser fra</p>
+                  <Card
+                      name={"Kalle's Klippere"}
+                      distance={3}
+                      availableTimeslots={13}
+                      price={99}
+                      website={"https://www.google.com/"}
+                  />
+                  <Card
+                      name={"KlipKlapperne"}
+                      distance={5}
+                      availableTimeslots={12}
+                      price={1324}
+                      website={"https://www.google.com/"}
+                  />
               </div>
               <div className="split right">
                   <div className="test up">
